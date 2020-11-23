23fdf4769bfd36c298518f302c51a12a7e53b7d9
owen <owen@cybersaur.io>
2020-11-23 13:59:40 +0000

    JARM runs
    Runs on server and parsed back to client upon request

--- api/app.py
+++ api/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api, Resource
+from jarmer import run
+
+from subprocess import call
+
 
 app = Flask(__name__)
 CORS(app)
@@ -8,7 +12,8 @@ api = Api(app)
 
 class generateJarm(Resource):
     def get(self, ip):
-        return {"jarm": ip}
+        jarm = run(ip)
+        return {"jarm": jarm}
 
 api.add_resource(generateJarm, "/api/<string:ip>")
 
